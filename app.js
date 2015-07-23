e29a92aa3c09de17ae4cc8f4d3c50af519bd834c
Austin Enright <abe8591@gmail.com>
2015-07-23 10:51:33 -0400

    Fixed PG error

--- app.js
+++ app.js
@@ -1,78 +1,117 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
 var pg = require('pg');
-var path = require('path');
 var fs = require('fs');
-var bodyParser = require('body-parser');
 
-var conString = "pg://postgres:111111@localhost:5432/todo";
+var port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+var conString = "pg://postgres:111111@149.24.49.145:5432/todo";
 var client = new pg.Client(conString);
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/views/index.html');
+app.listen(port, function(err) {
+	
+	 console.log('listening on *:3000');
 });
 
+function handler (req, res) {
+  fs.readFile(__dirname + '/client/index.html', function (err, data) {
+    if (err) {
+      throw err;
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
 io.on('connection', function(socket){
-  console.log('a user connected');
-  client.connect();
-  pg.connect(conString, function(err, client, done) {
+	console.log('a user connected');
+	
 	
 	socket.on('create', function(req) {
-		console.log(req.create1);
-		console.log(req.create2);
-		client.query(req.create1);
-		client.query(req.create2);
+		//client.connect();
+		pg.connect(conString, function(err, client, done) {
+			if(err) {
+				console.log(err);
+			} else {
+				console.log(req.create1);
+				console.log(req.create2);
+				client.query(req.create1);
+				client.query(req.create2);
+				done();
+			}
+		});
 	});
 	  
 	socket.on('insert', function(req) {
-		var results = [];
-		client.query(req.insert1).on("row", function (row) {
-			results.push(row);
+		//client.connect();
+		pg.connect(conString, function(err, client, done) {
+			if(err) {
+				console.log(err);
+			} else {
+				var results = [];
+				client.query(req.insert1).on("row", function (row) {
+					results.push(row);
+				});
+				console.log(req.insert1);
+				client.query(req.insert2).on("row", function (row) {
+					results.push(row);
+				});
+				console.log(req.insert2);
+				done();
+			}
 		});
-		console.log(req.insert1);
-		client.query(req.insert2).on("row", function (row) {
-			results.push(row);
-		});
-		console.log(req.insert2);
 	});
 	  
 	socket.on('select', function(req) {
 		var results = [];
-		//client.query(req).on("row", function (row) {
-			//results.push(row);
-		//});
-		
-		client.query(req, function (err, result) {
-			// On end JSONify and write the results to console and to HTML output
-			console.log(JSON.stringify(result, null, "    "));
-			socket.emit('table', result);
+		//client.connect();
+		pg.connect(conString, function(err, client, done) {
+			//client.query(req).on("row", function (row) {
+				//results.push(row);
+			//});
+			
+			client.query(req, function (err, result) {
+				// On end JSONify and write the results to console and to HTML output
+				if(err) {
+					console.log(err);
+				} else {
+					console.log(JSON.stringify(result, null, "    "));
+					socket.emit('table', result);
+					done();
+				}
+			});
 		});
 	});
 	  
 	socket.on('put', function(req) {
-		client.query(req);
-		console.log(req);
+		//client.connect();
+		pg.connect(conString, function(err, client, done) {
+			client.query(req, function (err, result) {
+				if(err) {
+					console.log(err);
+				} else {
+					console.log(JSON.stringify(result, null, "    "));
+					done();
+				}
+			});
+			console.log(req);
+		});
 	});
 
 	socket.on('delete', function(req) {
-		client.query(req);
-		console.log(req);
-	
-	});
-	
-	if(err) {
-	  console.log(err);
-	}
-  });
-});
+		//client.connect();
+		pg.connect(conString, function(err, client, done) {
+			client.query(req, function (err, result) {
+				if(err) {
+					console.log(err);
+				} else {
+					console.log(JSON.stringify(result, null, "    "));
+					done();
+				}
+			});
+			console.log(req);
+		});
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+	});
 });
