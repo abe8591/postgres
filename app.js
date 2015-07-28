24901012cce6b391096ccaa6669100d7ff6523d5
Austin Enright <abe8591@gmail.com>
2015-07-28 13:25:59 -0400

    New connect architecture

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var favicon = require('serve-favicon');
 
 var port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+pg.defaults.poolSize = 100;
+
 var conString = "pg://other_user:u_pick_it@52.2.3.184:5432/ellucian";
-var client = new pg.Client(conString);
+//var client = new pg.Client(conString);
+var client;
 
 http.listen(port, function(err) {
 	 console.log('listening on ' + port);
@@ -23,35 +26,46 @@ io.on('connection', function(socket){
 	console.log('a user connected');
 	
 	socket.on('create', function(req) {
+		client = new pg.Client(conString);
 		pg.connect(conString, function(error, client, done) {
-			var query = client.query(req.create1);
-			query.on('error', function(error) {
-				//handle the error
-				console.log(error);
-				done(client);
-				client = new pg.Client(conString);
-				error = "Create1 Termination";
-				socket.emit('err', error);
+			var query = client.query(req.create1, function (err, result) {
+				// Write out as HTML output
+				if(err) {
+					console.log(err);
+					done(client);
+					client = new pg.Client(conString);
+					error = "Incorrect syntax";
+					socket.emit('err', err);
+				} else {
+					done(client);
+					//client = new pg.Client(conString);
+				}
 			});
 			//done(client);
 		});
 		console.log(req.create1);
+		pg.end();
 		pg.connect(conString, function(error, client, done) {
-			var query = client.query(req.create2);
-			query.on('error', function(error) {
-				//handle the error
-				console.log(error);
-				done(client);
-				//client = new pg.Client(conString);
-				error = "Create2 Termination";
-				socket.emit('err', error);
+			var query = client.query(req.create2, function (err, result) {
+				// Write out as HTML output
+				if(err) {
+					console.log(err);
+					done(client);
+					//client = new pg.Client(conString);
+					error = "Incorrect syntax";
+					socket.emit('err', err);
+				} else {
+					done(client);
+				}
 			});
 			//done(client);
 		});
 		console.log(req.create2);
+		pg.end();
 	});
 	  
 	socket.on('insert', function(req) {
+		client = new pg.Client(conString);
 		var results = [];
 		pg.connect(conString, function(error, client, done) {
 			var query = client.query(req.insert1).on("row", function (row) {
@@ -69,6 +83,7 @@ io.on('connection', function(socket){
 			});
 			console.log(req.insert1);
 		});
+		pg.end();
 		pg.connect(conString, function(error, client, done) {
 			var query = client.query(req.insert2).on("row", function (row) {
 				results.push(row);
@@ -84,9 +99,11 @@ io.on('connection', function(socket){
 			});
 			console.log(req.insert2);
 		});
+		pg.end();
 	});
 	  
 	socket.on('select', function(req) {
+		client = new pg.Client(conString);
 		var results = [];
 		pg.connect(conString, function(err, client, done) {			
 			var query = client.query(req, function (err, result) {
@@ -103,41 +120,52 @@ io.on('connection', function(socket){
 				}
 			});
 		});
+		pg.end();
 	});
 	  
 	socket.on('put', function(req) {
+		client = new pg.Client(conString);
 		pg.connect(conString, function(error, client, done) {
-			var query = client.query(req);
-			query.on('error', function(error) {
-				//handle the error
-				console.log(error);
-				done(client);
-				//client = new pg.Client(conString);
-				error = "Incorrect syntax";
-				socket.emit('err', error);
+			var query = client.query(req, function (err, result) {
+				// Write out as HTML output
+				if(err) {
+					console.log(err);
+					done(client);
+					//client = new pg.Client(conString);
+					error = "Incorrect syntax";
+					socket.emit('err', err);
+				} else {
+					done(client);
+				}
 			});
 			console.log(req);
 		});
+		pg.end();
 	});
 
 	socket.on('delete', function(req) {
+		client = new pg.Client(conString);
 		pg.connect(conString, function(error, client, done) {
-			var query = client.query(req);
-			query.on('error', function(error) {
-				//handle the error
-				console.log(error);
-				done(client);
-				//client = new pg.Client(conString);
-				error = "Incorrect syntax";
-				socket.emit('err', error);
+			var query = client.query(req, function (err, result) {
+				// Write out as HTML output
+				if(err) {
+					console.log(err);
+					done(client);
+					//client = new pg.Client(conString);
+					error = "Incorrect syntax";
+					socket.emit('err', err);
+				} else {
+					done(client);
+					//client = new pg.Client(conString);
+				}
 			});
 			console.log(req);
 		});
+		pg.end();
 
 	});
 	
 	socket.on('disconnect', function () {
-		pg.end();
 		io.emit('user disconnected');
 	});
 });
